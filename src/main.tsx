7ef471c0d17c9f285d5e29c3908dc501c39fa41e
Access-S <saurabhcaccess@gmail.com>
2026-03-29 00:55:26 +0000

    move: ErrorBoundary.tsx to src/components/layout/

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/app/App";
-import { ErrorBoundary } from "@/components/ErrorBoundary";
+import { ErrorBoundary } from "@/components/layout/ErrorBoundary";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
