78ecfdf0388f757b1e97cd8ac2f172e464bcc752
Access-S <saurabhcaccess@gmail.com>
2026-03-29 05:19:23 +0000

    fix(purchase-orders): correct CreatePOPage filename case in barrel export

--- src/features/purchase-orders/index.ts
+++ src/features/purchase-orders/index.ts
@@ -2,7 +2,7 @@
 
 // ============== BLOCK 1: Exports ==============
 export { PurchaseOrdersPage } from './PurchaseOrdersPage';
-export { CreatePoPage } from './CreatePoPage';
+export { CreatePoPage } from './CreatePOPage';
 export * from './components';
 export * from './forms';
 export * from './modals';
